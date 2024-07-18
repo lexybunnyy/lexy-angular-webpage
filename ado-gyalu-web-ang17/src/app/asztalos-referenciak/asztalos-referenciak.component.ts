8044e22db5695d3b179f1699f152253664e9e60d
lexybunnyy <alexandra.cselyuszka@gmail.com>
2024-07-18 16:05:39 +0200

    adogyalu-ang17: add image viewer

--- ado-gyalu-web-ang17/src/app/asztalos-referenciak/asztalos-referenciak.component.ts
+++ ado-gyalu-web-ang17/src/app/asztalos-referenciak/asztalos-referenciak.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgImageSliderModule } from 'ng-image-slider';
 @Component({
   selector: 'app-asztalos-referenciak',
-  standalone: true,
-  imports: [],
+  standalone: true, 
+  imports: [
+    CommonModule,
+    NgImageSliderModule,
+  ],
   templateUrl: './asztalos-referenciak.component.html',
   styleUrl: './asztalos-referenciak.component.less'
 })
-export class AsztalosReferenciakComponent {
+export class AsztalosReferenciakComponent implements OnInit {
+  images: string[] = [
+    'assets/referenciak/konyhabutor/konyhabutor_1.jpg',
+    'assets/referenciak/konyhabutor/konyhabutor_2.jpg',
+    'assets/referenciak/konyhabutor/konyhabutor_3.jpg',
+    'assets/referenciak/konyhabutor/konyhabutor_1.jpg',
+    'assets/referenciak/konyhabutor/konyhabutor_2.jpg',
+    'assets/referenciak/konyhabutor/konyhabutor_3.jpg',
+    'assets/referenciak/konyhabutor/konyhabutor_1.jpg',
+    'assets/referenciak/konyhabutor/konyhabutor_2.jpg',
+    'assets/referenciak/konyhabutor/konyhabutor_3.jpg',
+  ];
 
+  imagesObject: Object[] = [];
+
+  onGaleryChange(event: any) {
+    console.log('galery', event);
+  }
+
+  ngOnInit(): void {
+    this.imagesObject = this.images.map(imageString => {
+      return {
+        image: imageString,
+        thumbImage: imageString
+      }
+    })
+  }
 }
+
+
